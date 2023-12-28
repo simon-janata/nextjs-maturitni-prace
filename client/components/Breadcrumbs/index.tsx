b119285153b32f9f1269df9d53684fd606315866
Schimanski04 <janata.simon04@gmail.com>
2023-12-28 16:38:02 +0100

    Set up Mantine UI in Next.js app.

---
+++ client/components/Breadcrumbs/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Breadcrumbs as BC } from "@mantine/core";
+
+const Breadcrumbs = ({ items }: { items: JSX.Element[] }) => {
+  return (
+    <BC aria-disabled>{items}</BC>
+  )
+}
+
+export default Breadcrumbs;
---
+++ client/components/Footer/index.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import { Container, Group, ActionIcon, rem } from "@mantine/core";
+import { IconBrandFacebook, IconBrandYoutube, IconBrandInstagram } from "@tabler/icons-react";
+import { MantineLogo } from "@mantinex/mantine-logo";
+import classes from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={classes.footer}>
+      <Container className={classes.inner}>
+        <MantineLogo size={28} />
+        <Group gap={0} className={classes.links} justify="flex-end" wrap="nowrap">
+          <ActionIcon component={Link} href="https://www.facebook.com/pslibofficial/" rel="noopener noreferrer" target="_blank" size="lg" color="gray" variant="subtle">
+            <IconBrandFacebook style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+          </ActionIcon>
+
+          <ActionIcon component={Link} href="https://www.instagram.com/pslib_official/" rel="noopener noreferrer" target="_blank" size="lg" color="gray" variant="subtle">
+            <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+          </ActionIcon>
+          
+          <ActionIcon component={Link} href="https://www.youtube.com/@prumkaTV" rel="noopener noreferrer" target="_blank" size="lg" color="gray" variant="subtle">
+            <IconBrandYoutube style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </div>
+  )
+}
+
+export default Footer;
