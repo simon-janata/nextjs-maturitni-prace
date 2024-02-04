84c99dc4c6e3a5df6a5fe8a3eb32699e31131ead
Schimanski04 <janata.simon04@gmail.com>
2024-02-04 10:22:13 +0100

    Added Features component with corresponding styles in Features.module.css and integrated it into the About page.

--- client/app/[locale]/about/page.tsx
+++ client/app/[locale]/about/page.tsx
@@ -1,15 +1,31 @@
 "use client";
 
-import UnderConstruction from "@/components/UnderConstruction";
+import Features from "@/components/Features";
+import { Button, Center, rem } from "@mantine/core";
 import { useDocumentTitle } from "@mantine/hooks";
+import { GithubIcon } from "@mantinex/dev-icons";
+import Link from "next/link";
 
 export default function AboutPage() {
   useDocumentTitle("About");
   
   return (
-    <UnderConstruction
-      pageName="About page"
-      description="There will be a section here that will display information about this web application and maybe some other information about the school and who knows what other information."
-    />
+    <>
+      <Features />
+    
+      <Center>
+        <Button
+          component={Link}
+          href="https://github.com/Schimanski04/nextjs-maturitni-prace"
+          target="_blank"
+          size="md"
+          mt="xl"
+          leftSection={<GithubIcon style={{ width: rem(20), height: rem(20) }} />}
+          style={{ backgroundColor: "var(--mantine-color-dark-9)", color: "#fff" }}
+        >
+          View Source Code
+        </Button>
+      </Center>
+    </>
   );
 }
---
+++ client/components/Features/index.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Text, SimpleGrid, Container, rem } from "@mantine/core";
+import { IconTruck, IconCertificate, IconCoin, IconCameraCog, IconCrop, IconHeartHandshake } from "@tabler/icons-react";
+import classes from "./Features.module.css";
+
+interface FeatureProps extends React.ComponentPropsWithoutRef<"div"> {
+  icon: React.FC<any>;
+  title: string;
+  description: string;
+}
+
+const Feature = ({ icon: Icon, title, description, className, ...others }: FeatureProps) => {
+  return (
+    <div className={classes.feature} {...others}>
+      <div className={classes.overlay} />
+
+      <div className={classes.content}>
+        <Icon style={{ width: rem(38), height: rem(38) }} className={classes.icon} stroke={1.5} />
+        <Text fw={700} fz="lg" mb="xs" mt={5} className={classes.title}>
+          {title}
+        </Text>
+        <Text c="dimmed" fz="sm">
+          {description}
+        </Text>
+      </div>
+    </div>
+  );
+}
+
+const mockdata = [
+  {
+    icon: IconCameraCog,
+    title: "Automated Photo Processing",
+    description:
+      "Automatically crops and names photographs of new students, saving teachers from having to do this manually.",
+  },
+  {
+    icon: IconCrop,
+    title: "Standardized Profile Pictures",
+    description:
+      "Applies a standard set of edits to each photo, ensuring that all profile pictures have a consistent appearance.",
+  },
+  {
+    icon: IconHeartHandshake,
+    title: "User-Friendly Interface",
+    description:
+      "Provides a simple, intuitive interface that makes it easy for teachers to upload photos, view the processed results, and make any necessary adjustments.",
+  },
+];
+
+const Features = () => {
+  const items = mockdata.map((item) => <Feature {...item} key={item.title} />);
+
+  return (
+    <Container mt={30} mb={30} size="lg">
+      <SimpleGrid cols={{ base: 1, sm: 3 }} spacing={50}>
+        {items}
+      </SimpleGrid>
+    </Container>
+  );
+}
+
+export default Features;
