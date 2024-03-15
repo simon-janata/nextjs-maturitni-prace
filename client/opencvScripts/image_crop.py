975026627bb51ec6c6f167043a0fc90da3a832e1
Schimanski04 <janata.simon04@gmail.com>
2024-03-15 03:08:59 +0100

    Refactored image detection and cropping in Python scripts and performed code cleanup.

--- client/opencvScripts/image_crop.py
+++ client/opencvScripts/image_crop.py
@@ -4,132 +4,66 @@ import sys
 import numpy as np
 import cv2
 
-# Read the image data from standard input
 image_bytes = sys.stdin.buffer.read()
 
-# Convert the image data to a NumPy array
 image_array = np.frombuffer(image_bytes, dtype=np.uint8)
 
-# Decode the image array
 img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
 
-# Check if the image file was read successfully
 if img is None:
     print("Failed to read image data")
     sys.exit(1)
 else:
-    print("Image data was read successfully")
-    # print(img.shape) # Print image shape
-
-    # Convert the image to grayscale for better processing
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Get the path to the current folder
     path_to_current_folder = os.path.dirname(os.path.realpath(__file__))
 
-    # Load the pre-trained Haar cascade classifier for face and eye detection
     face_cascade = cv2.CascadeClassifier(os.path.join(path_to_current_folder, 'haarcascade_frontalface_default.xml'))
     eye_cascade = cv2.CascadeClassifier(os.path.join(path_to_current_folder, 'haarcascade_eye.xml'))
-    # nose_cascade = cv2.CascadeClassifier(os.path.join(path_to_current_folder, 'haarcascade_mcs_nose.xml'))
 
-    # Detect faces in the image
-    # faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=7, minSize=(400, 400))
 
-    # Initialize the list to store distances and eye centers
     distances_to_eyes_top = []
-    eye_centers = []
 
-    # Loop through each face and detect eyes
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        distance_to_face_bottom = y + h
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
-        # Calculate the center of the face
         face_center_x = x + w // 2
-        eyes = eye_cascade.detectMultiScale(roi_gray, minSize=(50, 50))
+        eyes = eye_cascade.detectMultiScale(roi_gray, minNeighbors=7, minSize=(55, 55))
         for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
-            distance_to_eyes_top = y + ey
+            distance_to_eyes_top = y + ey + (eh // 2)
             distances_to_eyes_top.append(distance_to_eyes_top)
-            # Calculate the center of the eye and add it to the list
-            eye_center = x + ex + ew // 2
-            eye_centers.append(eye_center)
-            print("Distance from the top of the photo to the detected eyes:", distance_to_eyes_top)
+        break
 
-    # Calculate the average distance
     average_distance = sum(distances_to_eyes_top) / len(distances_to_eyes_top) if distances_to_eyes_top else 0
-    print("Average distance from the top of the photo to the detected eyes:", average_distance)
 
-    # Draw a horizontal line at the average distance from the top
-    cv2.line(img, (0, int(average_distance)), (img.shape[1], int(average_distance)), (0, 255, 0), 2)
+    difference = distance_to_face_bottom - average_distance
 
-    # Calculate the average x-coordinate of the eyes
-    average_eye_x = sum(eye_centers) // len(eye_centers) if eye_centers else 0
+    top_y = int((distance_to_face_bottom - 2.25 * difference) / 2)
+    top_y = max(0, min(img.shape[0], top_y))
 
-    # Draw a vertical line at the average x-coordinate of the eyes
-    cv2.line(img, (face_center_x, 0), (face_center_x, img.shape[0]), (0, 255, 0), 2)
-
-    # Calculate the height and width for the cropped image
-    crop_height = int(3 * average_distance)
+    crop_height = int(3 * (average_distance - top_y))
     crop_width = int(crop_height * (3 / 4))
 
-    # Calculate the starting x coordinate for the crop to center the image
-    # # start_x = int((img.shape[1] - crop_width) / 2)
-    # start_x = int(average_eye_x - (crop_width / 2))
-    # Calculate the starting x coordinate for the crop to center the image
     start_x = int(face_center_x - (crop_width / 2))
 
     if start_x < 0:
         start_x = 0
 
-    # start_y = mid_eye_point - crop_height // 2
-
-    # Crop the image based on the first detected face (if any)
     if len(faces) > 0:
-        (x, y, w, h) = faces[0] # Consider only the first detected face
-
-        # Calculate new width and height for 4:3 aspect ratio with 1400x1050 dimensions
-        # new_height = 1400
-        # new_width = int(new_height * 3 / 4)
-
-        # Ensure the cropping area fits within the detected face region
-        # start_x = max(0, x + (w - new_width) // 2)
-        # start_y = max(0, y + (h - new_height) // 2)
-
-        # cropped_image = img[y:y + h, x:x + w]
-        cropped_image = img[70:crop_height + 70, start_x:start_x + crop_width]
-        # cropped_image = img[start_y:start_y + new_height, start_x:start_x + new_width]
+        (x, y, w, h) = faces[0]
+        cropped_image = img[top_y:top_y + crop_height, start_x:start_x + crop_width]
     else:
         print("No faces detected")
         sys.exit(1)
 
-    # Cropping an image
-    # cropped_image = img[100:280, 150:330]
-        
-    # desired_height = 1400  # New height in pixels
-    # desired_width = 1050   # New width in pixels
-
-    # # Calculate the scaling factors for resizing
-    # scaling_factor_height = desired_height / cropped_image.shape[0]
-    # scaling_factor_width = desired_width / cropped_image.shape[1]
-
-    # # Choose the smallest scaling factor to maintain aspect ratio
-    # scaling_factor = min(scaling_factor_height, scaling_factor_width)
-
     resized_image = cv2.resize(cropped_image, (1050, 1400))
 
-    # Encode the cropped image to bytes
     retval, buffer = cv2.imencode(".JPG", resized_image)
     cropped_image_bytes = buffer.tobytes()
 
-    # Write the cropped image data to standard output
     sys.stdout.buffer.write(cropped_image_bytes)
-
-    # # Display the labeled image
-    # cv2.imshow("Face Detection and Labeling", resized_image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
 
     print("Cropped image data sent successfully")
     
--- client/opencvScripts/validate_and_resize_faces.py
+++ client/opencvScripts/validate_and_resize_faces.py
@@ -1,169 +1,37 @@
-# import cv2
-
-# # Load the pre-trained face detection model
-# face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
-# # Load the image
-# image_path = r"C:\Users\janat\source\repos\nextjs-maturitni-prace\client\opencvScripts\8people.jpg"
-# image = cv2.imread(image_path)
-
-# # Convert the image to grayscale
-# gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# # Detect faces in the image
-# # faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=6, minSize=(70, 70))
-# faces = face_cascade.detectMultiScale(gray_image, 1.1, 4, minSize=(400, 400))
-
-# # Draw rectangles around the faces and count them
-# face_count = 0
-# for (x, y, w, h) in faces:
-#     cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-#     faces = image[y:y + h, x:x + w] 
-#     cv2.imshow("face",faces) 
-#     # cv2.imwrite('face.jpg', faces) 
-#     # face_count += 1
-
-# # Display the number of faces
-# print("Number of faces detected:", face_count)
-
-# # Display the image with rectangles drawn around faces
-# cv2.imshow('Face Counter', image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
 import cv2
 import sys
 import numpy as np
 import json
 import base64
 
-# Read the image data from standard input
 image_bytes = sys.stdin.buffer.read()
 
-# Convert the image data to a NumPy array
 image_array = np.frombuffer(image_bytes, dtype=np.uint8)
 
-# Load the pre-trained face detection model
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Load the image
-# original_image_path = r"C:\Users\janat\source\repos\nextjs-maturitni-prace\client\opencvScripts\test13.jpg"
-# original_image = cv2.imread(original_image_path)
 original_image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
 
-# Check if the image file was read successfully
 if original_image is None:
     print("Failed to read image data")
     sys.exit(1)
 else:
-    # print("Image data was read successfully")
-    # Specify the desired dimensions
-    # desired_height = 1000  # New height in pixels
-    # desired_width = 750   # New width in pixels
-    # desired_height = 3080  # New height in pixels
-    # desired_width = 2320   # New width in pixels
-    # desired_height = 1540  # New height in pixels
-    # desired_width = 1160   # New width in pixels
-    # desired_height = original_image.shape[0] * (1 / 4)  # New height in pixels
-    # desired_width = original_image.shape[1] * (1 / 4)  # New width in pixels
-
-    # Calculate the scaling factors for resizing
-    # scaling_factor_height = desired_height / original_image.shape[0]
-    # scaling_factor_width = desired_width / original_image.shape[1]
-
-    # Choose the smallest scaling factor to maintain aspect ratio
-    # scaling_factor = min(scaling_factor_height, scaling_factor_width)
-
-    # Resize the image using the chosen scaling factor
     resized_image = cv2.resize(original_image, None, fx=(1 / 4), fy=(1 / 4), interpolation=cv2.INTER_AREA)
 
-
-
-    # # Calculate the scaling factors for resizing to halfway size
-    # scaling_factor_height = (desired_height / 2) / original_image.shape[0]
-    # scaling_factor_width = (desired_width / 2) / original_image.shape[1]
-
-    # # Choose the smallest scaling factor to maintain aspect ratio
-    # scaling_factor = min(scaling_factor_height, scaling_factor_width)
-
-    # # Resize the image to halfway size
-    # halfway_resized_image = cv2.resize(original_image, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_LINEAR)
-
-    # # Calculate the scaling factors for resizing to desired size
-    # scaling_factor_height = desired_height / halfway_resized_image.shape[0]
-    # scaling_factor_width = desired_width / halfway_resized_image.shape[1]
-
-    # # Choose the smallest scaling factor to maintain aspect ratio
-    # scaling_factor = min(scaling_factor_height, scaling_factor_width)
-
-    # # Resize the image to the desired size
-    # resized_image = cv2.resize(halfway_resized_image, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_LINEAR)
-
-
-
-
-    # resized_image = cv2.resize(original_image, (desired_width, desired_height))
-
-    # Save the resized image
-    # cv2.imwrite(r"C:\Users\janat\source\repos\nextjs-maturitni-prace\client\opencvScripts\resized_image.jpg", resized_image)
-
-    # Convert the image to grayscale
     gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 
-    # cv2.imshow("Face Detection and Labeling", gray_image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    # Detect faces in the image
     faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=7, minSize=(400, 400))
 
-    # Check if exactly one face was detected
     is_single_face = len(faces) == 1
 
-    # Print the number of detected faces and whether exactly one face was detected
-    print("Number of faces detected:", len(faces))
-    print("Is single face:", is_single_face)
-
-    # Process each detected face
-    for (x, y, w, h) in faces:
-        # Extract the region of interest (ROI) from the top of the hair to the shoulders
-        # top_hair_y = max(0, y - int(0.35 * h))
-        # shoulders_y = y + h
-        # roi = resized_image[top_hair_y:shoulders_y, x:x+w]
-        top_hair_y = y
-        shoulders_y = y + h
-        roi = resized_image[top_hair_y:shoulders_y, x:x+w]
-
-        # Draw a rectangle around the detected face
-        # cv2.rectangle(resized_image, (x, top_hair_y), (x+w, shoulders_y), (255, 0, 0), 2)
-        cv2.rectangle(resized_image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-        # Add a label indicating the face
-        cv2.putText(resized_image, "Face", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
-
-    # Encode the cropped image to bytes
     retval, buffer = cv2.imencode(".JPG", resized_image)
     resized_image_bytes = buffer.tobytes()
 
-    # Convert the image to base64
     resized_image_base64 = base64.b64encode(resized_image_bytes).decode()
 
-    # # Write the cropped image data and the boolean value to standard output
-    # sys.stdout.buffer.write(resized_image_bytes)
-    # sys.stdout.buffer.write(str(is_single_face).encode())
-
-    # Create a JSON object containing the image and the boolean value
     output = json.dumps({
         "image": resized_image_base64,
         "is_single_face": is_single_face
     })
 
     sys.stdout.buffer.write(output.encode("utf-8"))
-    # sys.stdout.buffer.write(json.dumps(output).encode())
-
-    # print(output)
-
-    # # Display the labeled image
-    # cv2.imshow("Face Detection and Labeling", resized_image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
